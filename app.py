2fc3fce3f9950c3681d1e70c4c7b786d4544556d
alok162004 <167989151+alok162004@users.noreply.github.com>
2025-05-12 22:56:39 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    email = request.form['email']
+    return f"User Registered Successfully!<br>Name: {name}<br>Email: {email}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+# git init
+# git add .
+# git push origin main
+# git push -u origin main
+# git remote remove origin  
+# git remote add origin https://github.com/YOUR_USERNAME/flask-ci-cd.git
+# git branch -M main
+# git push -u origin main
